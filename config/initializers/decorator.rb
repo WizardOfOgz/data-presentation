1ea486e9fa31165c11cdcd92092eba843a2b4d97
WizardOfOgz <andyogzewalla@gmail.com>
2014-03-11 15:54:21 -0500

    Code comment and example

--- config/initializers/decorator.rb
+++ config/initializers/decorator.rb
@@ -1,4 +1,8 @@
 module Decorator
+  # Injects decorator modules into objects.
+  # Example:
+  #   @user = User.find(123)
+  #   @user.decorate!         #=> +@user+ now has the decorator methods.
   def decorate!(decorator_name = self.class.name)
     singleton_class.class_eval do
       include("#{decorator_name}Decorator".constantize)
