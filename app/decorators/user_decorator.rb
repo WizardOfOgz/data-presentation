94478e18db6b3d98ac6da8cb76c490bcac0b4aca
WizardOfOgz <andyogzewalla@gmail.com>
2014-03-11 15:23:30 -0500

    Add age

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,9 +1,15 @@
 module UserDecorator
+  include ActionView::Helpers::DateHelper  # For +distance_of_time_in_words+
+
   def name  # New method
     [last_name, first_name].reject(&:blank?).join(', ')
   end
 
   def ssn  # Override model method
     super && "xxx-xx-#{super[-4..-1]}"
+  end
+
+  def age
+    distance_of_time_in_words(birthdate, Time.now)  if birthdate
   end
 end
